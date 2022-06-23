81df6a8069621ca0e2a65f64309e105b58099162
ADH <ADH@ADH-PC>
2022-06-23 16:29:43 +0430

    Paginiaition

--- src/pages/sale/customer/CustomerList.js
+++ src/pages/sale/customer/CustomerList.js
@@ -1,80 +1,132 @@
-import React, { useEffect, useState } from 'react'
-
+import React from 'react'
 import {
-  CButton,
   CCard,
   CCardBody,
   CCardHeader,
   CCol,
   CRow,
+  CButton,
 } from '@coreui/react'
-import { CustomTable } from 'src/custom-components/custom-grid/CustomTable';
-import {customerServices} from 'src/services/customerService'
+import { CustomTable } from 'src/custom-components/custom-grid/CustomTable'
+import { customerServices } from 'src/services/customerService'
 import { useNavigate } from 'react-router-dom';
+import CustomBootstrapTable from 'src/custom-components/custom-grid/CustomBootstrapTable'
+import CIcon from '@coreui/icons-react'
+import Swal from 'sweetalert2'
 
 const CustomerList = () => {
-  
-  const [data,setData]=React.useState([]);
+  const [data, setData] = React.useState([]);
+  const [currentPage, setPage] = React.useState(1);
+  const [sizePerPage, setSizePerPage] = React.useState(1);
+  const [totalSize, setTotalSize] = React.useState(1);
   const navigate = useNavigate();
-  
+
   const columns = React.useMemo(
     () => [
       {
-        Header: 'کد مشتری',
-        accessor: 'customerCode',
+        text: 'ردیف',
+        dataField: 'id',
       },
       {
-        Header: 'نام',
-        accessor: 'firstName',
+        text: 'کد مشتری',
+        dataField: 'customerCode',
       },
       {
-        Header: 'نام خانوادگی',
-        accessor: 'lastName',
+        text: 'نام',
+        dataField: 'firstName',
       },
       {
-        Header: 'شماره تماس',
-        accessor: 'mobile',
+        text: 'نام خانوادگی',
+        dataField: 'lastName',
       },
       {
-        Header: 'استان',
-        accessor: 'provinceName',
+        text: 'شماره تماس',
+        dataField: 'mobile',
       },
       {
-        Header: 'شهر',
-        accessor: 'cityName',
+        text: 'استان',
+        dataField: 'provinceName',
+      },
+      {
+        text: 'شهر',
+        dataField: 'cityName',
+      }, 
+      {
+        dataField: 'inStock',
+        text: 'In Stock',
+        formatter: (cellContent, row) => (
+          <div>
+            <CButton onClick={() => navigate('/sale/customers/edit/' + row.id)} color='light' size="sm" shape="rounded-pill">
+              {/* <CIcon name="cil-pencil" className="me-2" /> */}
+              ویرایش
+            </CButton>
+
+            
+            <CButton onClick={async () => await removeItem(row.id)} color='danger' size="sm" shape="rounded-pill">
+              {/* <CIcon name="cil-drop" className="me-2" /> */}
+              حذف
+            </CButton>
+          </div>
+        )
       },
     ],
-  )
+  );
 
-  // const data = React.useMemo(() => [
-  //   {firstName:'ali',lastName:'rezaie',age:'25'},
-  //   {firstName:'ali',lastName:'rezaie',age:'25'},
-  //   {firstName:'ali',lastName:'rezaie',age:'25'},
-  //   {firstName:'ali',lastName:'rezaie',age:'25'},
-  // ]);
-
-  React.useEffect(async() => {
-    const result=await customerServices.getAll(1);
-    setData(result);
+  React.useEffect(async () => {
+    await getData();
   }, [])
-   
+
+  const getData = async (page = 1, itemCount = 2) => {
+    const result = await customerServices.getAll({ page, itemCount });
+    setData(result.data);
+    setPage(result.page);
+    setSizePerPage(result.sizePerPage);
+    setTotalSize(result.totalSize);
+  }
+
+  const removeItem = async (id) => {
+
+    Swal.fire({
+      title: 'در صورت حذف قابل بازیابی نمی باشد',
+      showCancelButton: true,
+      confirmButtonText: `تایید`,
+      cancelButtonText: `انصراف`,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await customerServices.delete(id);
+        await getData(currentPage, sizePerPage);
+      }
+    })
+  }
+
+  const handleTableChange = async (type, { page, sizePerPage }) => {
+    await getData(page, sizePerPage);
+  }
 
   return (
     <CRow>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardHeader>
-            لیست مشتریان-
+            لیست مشتریان
           </CCardHeader>
           <CCardBody>
-            <CButton color='light' size='sm' onClick={()=> navigate('/sale/customers/add')}>
-              افزودن جدید
+            <CButton color="light" size="sm" onClick={() => navigate("/sale/customers/add")}>
+              افزودن آیتم جدید
             </CButton>
-            <CustomTable columns={columns} data={data} />
-           </CCardBody>
+            <CustomBootstrapTable
+              columns={columns}
+              data={data}
+              page={currentPage}
+              sizePerPage={sizePerPage}
+              totalSize={totalSize}
+              onTableChange={handleTableChange}
+            />
+          </CCardBody>
         </CCard>
       </CCol>
     </CRow>
   )
 }
+
 export default CustomerList
